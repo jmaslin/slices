4c1fc2c92cf93d9c0e908fb3ed3b747b0009826c
Justin <me@justinmaslin.com>
2015-08-13 21:24:01 -0400

    Simple pizza list

--- slices.js
+++ slices.js
@@ -1,19 +1,13 @@
+Pizzas = new Mongo.Collection("pizzas");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    pizzas: function () {
+      return Pizzas.find({});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
