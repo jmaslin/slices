9f3c25860314e88f38d8ee4d0f8479ec4a57d049
Andrew Rudoi <andrewrudoi@Andrews-MacBook-Pro.local>
2015-08-15 20:14:43 -0400

    worldwide pizza map

--- slices.js
+++ slices.js
@@ -86,40 +86,31 @@ if (Meteor.isClient) {
   Template.map.onCreated(function() {  
   var self = this;
 
-  GoogleMaps.ready('map', function(map) {
-    var marker;
+  if (Session.get('pizzaName')) {
+    GoogleMaps.ready('map', function(map) {
+    var latLng = Geolocation.latLng();
 
-    var image = {
-      url: "images/slice-transparent.png",
-      size: new google.maps.Size(50, 50),
-      origin: new google.maps.Point(0,0),
-      anchor: new google.maps.Point(25,25)
-    }
-
-    // Create and move the marker when latLng changes.
-    self.autorun(function() {
-      var latLng = Geolocation.latLng();
-      if (! latLng)
-        return;
-
-      // If the marker doesn't yet exist, create it.
-      if (! marker) {
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(latLng.lat, latLng.lng),
-          map: map.instance,
-          icon: image
-        });
-      }
-      // The marker already exists, so we'll just change its position.
-      else {
-        marker.setPosition(latLng);
-      }
-
-      // Center and zoom the map view onto the current position.
-      map.instance.setCenter(marker.getPosition());
-      map.instance.setZoom(MAP_ZOOM);
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(latLng.lat, latLng.lng),
+      map: map.instance
     });
   });
+  }
+  else {
+    GoogleMaps.ready('map', function(map) {
+      var mapPizzas = Pizzas.find().fetch();
+
+      for (i = 0; i < mapPizzas.length; i++) {
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(mapPizzas[i].location.latitude, mapPizzas[i].location.longitude),
+          map: map.instance
+        });
+
+        console.log(mapPizzas[i].location);
+      }
+
+    });
+  }
 });
 
   Template.pizza.events({
